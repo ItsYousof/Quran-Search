3638114a4319eeb26e402790caa5a9aa4e63a58a
Yousof <157152463+ItsYousof@users.noreply.github.com>
2024-07-12 18:09:15 -0400

    updated font & JS fixes

--- js/script.js
+++ js/script.js
@@ -22,39 +22,43 @@ function createResult(word, meaning, num, surah) {
     resultsContainer.appendChild(result);
 }
 
+async function getWord() {
+    const word = wordInput.value.trim();
+    if (word === '') {
+        wordInput.style.borderColor = 'red';
+        setTimeout(() => {
+            wordInput.style.borderColor = '#ced4da';
+        }, 2000);
+        return;
+    }
+
+    const response = await fetch('../words.json'); // Replace with the actual path to your JSON file
+    const data = await response.json();
+
+    // Preprocess data to store both the original word and the word without Tashkeel
+    const processedData = data.map(entry => ({
+        ...entry,
+        wordWithoutTashkeel: removeTashkeel(entry.word)
+    }));
+
+    const normalizedWord = removeTashkeel(word);
+
+    const results = processedData.filter(entry => entry.wordWithoutTashkeel === normalizedWord);
+
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+    if (results.length === 0) {
+        resultsContainer.innerHTML = '<p>لم يتم العثور على نتائج</p>';
+    } else {
+        results.forEach(entry => {
+            createResult(entry.word, entry.meaning, entry.ayaNumber, entry.surahName);
+        });
+    }
+}
+
 // Event listener for 'Enter' key press
 wordInput.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
-        const word = wordInput.value.trim();
-        if (word === '') {
-            wordInput.style.borderColor = 'red';
-            setTimeout(() => {
-                wordInput.style.borderColor = '#ced4da';
-            }, 2000);
-            return;
-        }
-
-        const response = await fetch('../words.json'); // Replace with the actual path to your JSON file
-        const data = await response.json();
-
-        // Preprocess data to store both the original word and the word without Tashkeel
-        const processedData = data.map(entry => ({
-            ...entry,
-            wordWithoutTashkeel: removeTashkeel(entry.word)
-        }));
-
-        const normalizedWord = removeTashkeel(word);
-
-        const results = processedData.filter(entry => entry.wordWithoutTashkeel === normalizedWord);
-
-        resultsContainer.innerHTML = ''; // Clear previous results
-
-        if (results.length === 0) {
-            resultsContainer.innerHTML = '<p>لم يتم العثور على نتائج</p>';
-        } else {
-            results.forEach(entry => {
-                createResult(entry.word, entry.meaning, entry.ayaNumber, entry.surahName);
-            });
-        }
+        getWord();
     }
 });
