0fb8af74032d25ce7ad6620a71b7979711979a4a
Yousof <157152463+ItsYousof@users.noreply.github.com>
2024-07-27 23:06:52 -0400

    PLEASE!!!

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,26 @@
+const CACHE_NAME = 'sirat-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/style.css',
+  '/js/script.js',
+  // Add other resources you want to cache
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('static-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles/style.css',
-          '/js/script.js',
-          '/android-icon-36x36.png',
-          '/android-icon-48x48.png',
-          '/android-icon-72x72.png',
-          '/android-icon-96x96.png',
-          '/android-icon-144x144.png',
-          '/android-icon-192x192.png'
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
         return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
